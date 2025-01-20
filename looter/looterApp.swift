156cff8dad420085770c56a216a91e8f3dcaa751
Nicolas <NT.Aergie@gmail.com>
2025-01-20 08:49:47 +0100

    Initial Commit

---
+++ looter/looterApp.swift
@@ -0,0 +1,17 @@
+//
+//  looterApp.swift
+//  looter
+//
+//  Created by Nicolas TREHOU on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct looterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
